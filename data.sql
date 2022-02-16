e3c25f2d3746dc354dbe24730fcc5bd6ba4d097d
Anjacodes <anja_schmidt7@posteo.de>
2022-02-16 15:46:22 -0600

    Add Join Table For Visits

--- data.sql
+++ data.sql
@@ -3,27 +3,18 @@
 /* MILESTONE 1 */
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Agumon', '2020-02-03', 0, TRUE, 10.23);
-
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Gabumon', '2018-11-15', 2, TRUE, 8.0);
-
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Pikachu', '2021-01-07', 1, FALSE, 15.04);
-
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Devimon', '2017-05-12', 5, TRUE, 11.0);
 
 /* MILESTONE 2 */
 
 /*INSERT MORE VALUES INTO ANIMALS TABLE*/
-
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Charmander', '2020-02-08', 0, FALSE, -11);
-
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Plantmon', '2022-11-15', 2, TRUE, -5.7);
-
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Squirtle', '1993-04-02', 3, FALSE, -12.13);
-
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Angemon', '2005-06-12', 1, TRUE, -45);
-
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Boarmon', '2005-06-7', 7, TRUE, -20.4);
-
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Blossom', '1998-10-13', 3, TRUE, 17);
 
 /* MILESTONE 3 */
@@ -54,3 +45,39 @@ UPDATE animals SET owner_id = 2 WHERE name = 'Gabumon' OR name = 'Pikachu';
 UPDATE animals SET owner_id = 3 WHERE name = 'Devimon' OR name = 'Plantmon';
 UPDATE animals SET owner_id = 4 WHERE name = 'Charmander' OR name = 'Squirtle' OR name = 'Blossom';
 UPDATE animals SET owner_id = 5 WHERE name = 'Angemon' OR name = 'Boarmon';
+
+/* MILESTONE 4 */
+
+/* ADD DATA TO VETS TABLE */
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('William Tatcher', 45, '2000-04-23');
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('Maisy Smith', 26, '2019-01-17');
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('Stephanie Mendez', 64, '1981-05-04');
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('Jack Harkness', 38, '2008-06-08');
+
+/* ADD DATA TO SPECIALIZATIONS */
+INSERT INTO specializations (vet_id, species_id) VALUES (1,1);
+INSERT INTO specializations (vet_id, species_id) VALUES (3,1);
+INSERT INTO specializations (vet_id, species_id) VALUES (3,2);
+INSERT INTO specializations (vet_id, species_id) VALUES (4,2);
+
+/* ADD DATA TO VISITS */
+INSERT INTO visits VALUES (1,1,'2020-05-24');
+INSERT INTO visits VALUES (1,3,'2020-07-22');
+INSERT INTO visits VALUES (2,4,'2021-02-02');
+INSERT INTO visits VALUES (3,2,'2020-01-05');
+INSERT INTO visits VALUES (3,2,'2020-03-08');
+INSERT INTO visits VALUES (3,2,'2020-05-14');
+INSERT INTO visits VALUES (4,3,'2021-05-04');
+INSERT INTO visits VALUES (5,4,'2021-02-24');
+INSERT INTO visits VALUES (11,2,'2019-12-21');
+INSERT INTO visits VALUES (11,1,'2020-08-10');
+INSERT INTO visits VALUES (11,2,'2021-04-07');
+INSERT INTO visits VALUES (7,3,'2019-09-29');
+INSERT INTO visits VALUES (8,4,'2020-10-03');
+INSERT INTO visits VALUES (8,4,'2020-11-04');
+INSERT INTO visits VALUES (9,2,'2019-01-24');
+INSERT INTO visits VALUES (9,2,'2019-05-15');
+INSERT INTO visits VALUES (9,2,'2020-02-27');
+INSERT INTO visits VALUES (9,2,'2020-08-03');
+INSERT INTO visits VALUES (10,3,'2020-05-24');
+INSERT INTO visits VALUES (10,1,'2021-01-11');
--- schema.sql
+++ schema.sql
@@ -31,3 +31,14 @@ ALTER TABLE animals ADD COLUMN species_id INT;
 ALTER TABLE animals ADD CONSTRAINT species_constraint FOREIGN KEY (species_id) REFERENCES species;
 ALTER TABLE animals ADD COLUMN owner_id INT;
 ALTER TABLE animals ADD CONSTRAINT owner_constraint FOREIGN KEY (owner_id) REFERENCES owners;
+
+/* MILESTONE 4 */
+
+/* CREATE VETS TABLE */
+CREATE TABLE vets (id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, name VARCHAR(250), age INT, date_of_graduation DATE);
+
+/* CREATE SPECIALIZATIONS TABLE */
+CREATE TABLE specializations (vet_id INT, species_id INT, FOREIGN KEY(vet_id) REFERENCES vets(id), FOREIGN KEY(species_id) REFERENCES species(id), PRIMARY KEY(vet_id, species_id));
+
+/* CREATE VISITS TABLE */
+CREATE TABLE visits (animal_id INT, vet_id INT, date_of_visit DATE, FOREIGN KEY(animal_id) REFERENCES animals(id), FOREIGN KEY(vet_id) REFERENCES vets(id), PRIMARY KEY(animal_id, vet_id));
